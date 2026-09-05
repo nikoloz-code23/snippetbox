1e93f427bf5100f86b5e76a56180ddc60e8037a1
nikoloz-code23 <73937823+nikoloz-code23@users.noreply.github.com>
2026-09-05 05:48:04 +0400

    Update snippetView to render an HTML with the proper snippet.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	//"html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -14,43 +14,6 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	/*
-	// Initialize a slice containing the paths to the two files.
-	// It's important to note that the file containing our base
-	// template must be the *first* file in the slice.
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
-
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message, use
-	// the http.Error() function to send an Internal Server Error response to the
-	// user, and then return from the handler so no subsequent code is executed.
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	// Then we use the Execute() method on the template set to write the
-	// template content as the response body. The last parameter to Execute()
-	// represents any dynamic data that we want to pass in, which for now we'll
-	// leave as nil.
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	log.Print(err.Error())
-	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	// }
-
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
-	*/
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
@@ -79,7 +42,32 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	// Initialize a slice containing the paths to the two files.
+	// It's important to note that the file containing our base
+	// template must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/view.tmpl.html",
+	}
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message, use
+	// the http.Error() function to send an Internal Server Error response to the
+	// user, and then return from the handler so no subsequent code is executed.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	data := newTemplateData(snippet)
+
+	// And then execute them by passing the dynamic data.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -99,7 +87,3 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
-
-// TODO: Figure out why the hell your .envrc isn't loading
-// TODO: Read the entirety of chapter 4 and take more notes. Research more into
-// 			 SQL DB stuff.
